6453b3ca292d3c34a4cbf01f64aa0e977e9b119c
Reed Dunkle <reeddunkle@gmail.com>
2016-05-31 17:48:30 -0400

    Edited karma.conf.js to only load the test files - there was a dependency error. Added webpack configuration to the karma.conf.js file. Got karma tests running.

--- karma.conf.js
+++ karma.conf.js
@@ -20,7 +20,6 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      // 'src/js/*.js',
       'src/js/*_test.js'
     ],
 
@@ -33,7 +32,6 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-        'src/js/*.js': ['webpack'],
         'src/js/*_test.js': ['webpack']
     },
 
